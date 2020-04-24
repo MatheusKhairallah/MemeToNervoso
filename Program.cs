9424c28e3dd57e4e7906d494cbbe04e9f17eb351
MatheusKhairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-04-24 20:36:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Usuário, o que você pensa sobre mim?: ");
+            string opiniao=Console.ReadLine();
+            string opiniao2=opiniao.Remove(10);
+
+            Console.Clear();
+
+            string mensagemOpiniao2=" " +opiniao2+ "... nao pera, to nervoso";
+
+            Console.WriteLine(mensagemOpiniao2);
+
+          
+           
+        }
+    }
+}
